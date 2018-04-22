b270e4ce7776ea36f71b97db08084a7f4f5635f0
Glitch (cultured-priest) <none>
2018-04-22 07:53:57 +0000

    🐌🏋 Checkpoint ./views/index.html:852327/407 ./server.js:852327/668 ./package.json:852327/2623 ./src/views/index.ejs:852327/1722

--- server.js
+++ server.js
@@ -4,16 +4,34 @@
 // init project
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+ 
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
+
+app.set('views', './src/views/');
+app.set('view engine', 'ejs');
+app.set('json spaces', 2);
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", function (req, res) {
+  res.send(req.body);
+  // res.render('index');
 });
 
 // listen for requests :)
