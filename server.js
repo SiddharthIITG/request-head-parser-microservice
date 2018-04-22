0edb2859e46e2bb2280eecafaaa9365596f58c9e
Glitch (cultured-priest) <none>
2018-04-22 20:31:48 +0000

    🙍🏥 Checkpoint ./server.js:852327/1350

--- server.js
+++ server.js
@@ -12,23 +12,11 @@ const ejs = require('ejs');
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-// parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: false }));
- 
-// parse application/json
-// app.use(bodyParser.json());
- 
+
 // app.set('views', './src/views/');
 // app.set('view engine', 'ejs');
 // app.set('json spaces', 2);
 
-// // http://expressjs.com/en/starter/basic-routing.html
-// app.use("/", function (req, res, next) {
-//   // res.setHeader("Content-Type", "application/json");
-//   res.send(req.body);
-//   next();
-//   // res.render('index');
-// });
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -37,10 +25,15 @@ app.use(bodyParser.json())
 
 app.use(function (req, res) {
   res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n');
+  res.write('Information:\n\n');
   const regexp = /\(([^()]+)\)/;
-  ip = ip.substr(7)
-  res.end(JSON.stringify({os: req.headers['user-agent'].match(regexp)[1], ip: req.ipsubstr(0, 7) == "::ffff:" ? }, null, 2))
+  var ip = (req.headers['x-forwarded-for'] ||
+     req.connection.remoteAddress ||
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress).split(",")[0];
+  if(ip.substr(0,7) == "::ffff:")
+    ip = ip.substr(7);
+  res.end(JSON.stringify({os: req.headers['user-agent'].match(regexp)[1], ip: ip, lang: req.headers["accept-language"]}, null, 2))
 })
 
 
